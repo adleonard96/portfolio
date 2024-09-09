79217fb7a3e5cb096a17cc4cc4302989fa7536db
adleonard96 <alexanderleonard27@gmail.com>
2024-09-08 22:49:34 -0400

    Started creating page that dynamically displays recent github repos

---
+++ Models/Github.cs
@@ -0,0 +1,32 @@
+namespace Github{
+    class GithubStats 
+    {
+        public string id {get; set;}
+        public string type {get; set;}
+        public Actor actor {get; set;}
+        public Repo  repo {get; set;}
+        public Payload payload {get; set;}
+        public bool publicRepo {get; set;}
+    }
+
+    class Actor
+    {
+
+    }
+
+    class Repo 
+    {
+
+    }
+
+    class Payload
+    {
+
+    }
+
+    class Commit
+    {
+
+    }
+}    
+    
