0fa2b220ca14c18084dbeab9e791bb2c8f9ee03d
adleonard96 <alexanderleonard27@gmail.com>
2024-09-10 21:25:21 -0400

    created class structure for Github API response

--- Models/Github.cs
+++ Models/Github.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+
 namespace Github{
     class GithubStats 
     {
@@ -11,22 +13,54 @@ namespace Github{
 
     class Actor
     {
+        public int id {get; set;}
+        
+        public string login {get; set;}
 
+        public string display_login {get; set;}
+
+        public string gravatar_id {get; set;}
+
+        public string url {get; set;}
+
+        public string avatar_url {get; set;}
     }
 
     class Repo 
     {
+        public int id {get; set;}
 
+        public string name {get; set;}
+
+        public string url { get; set;}
     }
 
     class Payload
     {
+        public int repository_id {get; set;}
+        public int push_id {get; set;}
+        public int size {get; set;}
+        public int distinct_size {get; set;}
+        public string ref_ {get; set;}
+        public string head {get; set;}
+        public string before {get; set;}
 
+        public ArrayList commits {get; set;}
     }
 
     class Commit
     {
+        public string sha {get; set;}
+        public Author author{get; set;}
+        public string message {get; set;}
+        public bool distinct {get; set;}
+        public string url {get; set;}
+    }
 
+    class Author
+    {
+        public string email {get; set;}
+        public string name {get; set;}
     }
 }    
     
