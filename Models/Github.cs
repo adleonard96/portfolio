70dcd2184a1cd834c173320da86391db55c431d6
adleonard96 <alexanderleonard27@gmail.com>
2024-09-11 22:49:21 -0400

    Added Github API and de-serialized it into objects

--- Models/Github.cs
+++ Models/Github.cs
@@ -1,6 +1,33 @@
-using System.Collections;
+using System.Text.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace Github{
+    class GithubStatRetriever
+    {
+        public static async Task<List<GithubStats>> GetStats()
+        {
+            const string USERNAME = "adleonard96";
+            List<GithubStats> stats = [];
+            var json = await GithubStatRetriever._GetDataFromGh(USERNAME);
+            stats.AddRange(JsonSerializer.Deserialize<List<GithubStats>>(json));
+            return stats;
+        }
+
+        private static async Task<string> _GetDataFromGh(string username)
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(
+                HttpMethod.Get,
+                string.Format("https://api.github.com/users/{0}/events/public", username)
+            );
+            request.Headers.Add("User-Agent", "Request");
+            var response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+
     class GithubStats 
     {
         public string id {get; set;}
@@ -8,7 +35,9 @@ namespace Github{
         public Actor actor {get; set;}
         public Repo  repo {get; set;}
         public Payload payload {get; set;}
+        [JsonPropertyName("public")]
         public bool publicRepo {get; set;}
+        public DateTime created_at {get; set;}
     }
 
     class Actor
@@ -38,14 +67,21 @@ namespace Github{
     class Payload
     {
         public int repository_id {get; set;}
-        public int push_id {get; set;}
+        public long push_id {get; set;}
         public int size {get; set;}
         public int distinct_size {get; set;}
-        public string ref_ {get; set;}
+
+        [JsonPropertyName("ref")]
+        public string payloadRef {get; set;}
         public string head {get; set;}
         public string before {get; set;}
 
-        public ArrayList commits {get; set;}
+        public List<Commit> commits {get; set;}
+        public string ref_type {get; set;}
+        public string master_branch {get; set;}
+        public string description { get; set; }
+        public string pusher_type { get; set; }
+        public string action { get; set; }
     }
 
     class Commit
